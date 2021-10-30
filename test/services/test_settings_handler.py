784a195e76a65b41a8d35fa78a3d10f5040f260e
macnietzsche <mbvillegas94@outlook.com>
2021-10-30 16:02:27 -0500

    REFACTOR: Refactored tests

--- test/services/test_settings_handler.py
+++ test/services/test_settings_handler.py
@@ -6,51 +6,43 @@ with open("test/config/settings.json") as file:
         settings_data = json_load(file)
 
 class TestSettingsHandler(TestCase):
-    def test_setting_is_missing_one_parameter(self):
+    def test_setting_is_missing_parameters(self):
         setting = settings_data['payment_config_missing_one_parameter']
         with self.assertRaises(Exception):
             SettingsHandler(setting)
 
-    def test_setting_is_missing_two_parameters(self):
         setting = settings_data['payment_config_missing_two_parameters']
         with self.assertRaises(Exception):
             SettingsHandler(setting)
 
-    def test_setting_is_missing_three_parameters(self):
         setting = settings_data['payment_config_missing_three_parameters']
         with self.assertRaises(Exception):
             SettingsHandler(setting)
-    
-    def test_setting_is_missing_all_parameters(self):
+
         setting = settings_data['payment_config_missing_all_parameters']
         with self.assertRaises(Exception):
             SettingsHandler(setting)
 
-    def test_setting_is_missing_random_parameters(self):
         setting = settings_data['payment_config_missing_random_parameters']
         with self.assertRaises(Exception):
             SettingsHandler(setting)
 
-    def test_setting_has_overlapping_time_intervals_case1(self):
+    def test_setting_has_overlapping_time_intervals(self):
         setting = settings_data["payment_config_overlapping_time_intervals_case1"]
         with self.assertRaises(Exception):
             SettingsHandler(setting)
 
-    def test_setting_has_overlapping_time_intervals_case2(self):
         setting = settings_data["payment_config_overlapping_time_intervals_case3"]
-        with self.assertRaises(Exception):
-            SettingsHandler(setting)
-    
-    def test_setting_has_overlapping_time_intervals_case3(self):
-        setting = settings_data["payment_config_overlapping_time_intervals_case3"]
-        with self.assertRaises(Exception):
-            SettingsHandler(setting)
-    
-    def test_setting_has_overlapping_time_intervals_case4(self):
-        setting = settings_data["payment_config_overlapping_time_intervals_case4"]
         with self.assertRaises(Exception):
             SettingsHandler(setting)
         
+        setting = settings_data["payment_config_overlapping_time_intervals_case3"]
+        with self.assertRaises(Exception):
+            SettingsHandler(setting)
+        
+        setting = settings_data["payment_config_overlapping_time_intervals_case4"]
+        with self.assertRaises(Exception):
+            SettingsHandler(setting)
 
             
         
