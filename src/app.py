600b1a86544aaa10b9c666176aedde38984cc69f
macnietzsche <mbvillegas94@outlook.com>
2021-10-30 14:40:17 -0500

    REFACTOR: TDD Green. Correct settings

--- src/app.py
+++ src/app.py
@@ -1,5 +1,2 @@
-import yaml
-
 if __name__ == '__main__':
-    with open('config.yml', 'r') as file
-...    prime_service = yaml.safe_load(file)
+    print("hello world")
--- src/services/settings_handler.py
+++ src/services/settings_handler.py
@@ -1,3 +1,5 @@
+
+REQUIRED_PARAMETERS = ['start_time','end_time', 'payment_amount_on_weekdays','payment_amount_on_weekends']
 class SettingsHandler():
     def __init__(self,settings) -> None:
         self.validated_settings = settings
@@ -8,11 +10,7 @@ class SettingsHandler():
 
     @validated_settings.setter
     def validated_settings(self,settings):
-        return None
-
-
-    
-
-
-    
-
+        for setting in settings:
+            for parameter in REQUIRED_PARAMETERS:
+                if not parameter in setting:
+                    raise Exception(f'Parameter {parameter} is missing.')
