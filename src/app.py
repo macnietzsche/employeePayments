90ccca818f1b066b04a0ad7b0015331e9fc0aa4d
macnietzsche <mbvillegas94@outlook.com>
2021-11-02 02:27:44 -0500

    REFACTOR: App integration finished

--- src/app.py
+++ src/app.py
@@ -1,26 +1,17 @@
-from json import load as json_load
-from subdomains.payment_handler import PaymentHandler
+from domain.employee import Employee
 from services.input_handler import InputHandler
 
 if __name__ == '__main__':
-    with open("test/input_files/payment_settings_test.json") as file:
-        payment_settings_data = json_load(file)
+    file = open("employees_input.txt", "r")
+    for count,line in enumerate(file):
+        try:
+            input_handler_service = InputHandler(line)
+            name = input_handler_service.normalized_input['label']
+            worked_days_summary = input_handler_service.normalized_input['value']
 
-    with open("test/input_files/worked_days_summaries_test.json") as file:
-        worked_days_summaries = json_load(file)
+            employee = Employee(name,worked_days_summary)
+            employee.print_payment_receipt()
+        except Exception as e:
+            print(f"Unable to process line {count + 1}. Error message: {str(e)}")
 
-    incorrect_inputs=[]
-    file = open("test/input_files/incorrect_input_test.txt", "r")
-    for line in file:
-        incorrect_inputs.append(line)
-
-    correct_inputs=[]
-    file = open("test/input_files/correct_input_test.txt", "r")
-    for line in file:
-        correct_inputs.append(line)
-
-    input = correct_inputs[0]
-    example = InputHandler(input)
-    test = example.normalized_input
-
-    print("hello")
+        
---
+++ src/domain/employee.py
@@ -0,0 +1,18 @@
+
+from subdomains.payment_handler import PaymentHandler
+from json import load as json_load
+
+with open("config/payment_settings.json") as file:
+    payment_settings_data = json_load(file)
+
+class Employee():
+    def __init__(self, name, worked_days_summary):
+        self.name = name
+        self.worked_days_summary = worked_days_summary
+        self.payment_handler_service = PaymentHandler(payment_settings_data)
+
+    def calculate_amount_to_be_paid(self):
+        return self.payment_handler_service.calculate_payment_amount(self.worked_days_summary)
+
+    def print_payment_receipt(self):
+        print(f"The amount to pay {self.name} is: {self.calculate_amount_to_be_paid()}")
